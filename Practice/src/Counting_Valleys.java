26b8af308836c5e6bb97e7c1a4b08f8405aa98c4
Lakshmikant Anandkumar Deshpande <lakshmikantdeshpande@gmail.com>
2017-04-07 17:26:13 +0530

    Counting Valleys solved

---
+++ Practice/src/Counting_Valleys.java
@@ -0,0 +1,41 @@
+import java.io.BufferedReader;
+import java.io.InputStreamReader;
+import java.io.PrintWriter;
+
+public class Counting_Valleys {
+
+	static BufferedReader br = new BufferedReader(new InputStreamReader(System.in));
+	static PrintWriter pw = new PrintWriter(System.out);
+
+	public static void main(String args[]) throws Exception {
+		int N = Integer.parseInt(br.readLine());
+
+		int count = 0;
+		int last = 0;
+		int seaLevel = 0;
+
+		String line = br.readLine();
+
+		for (int i = 0; i < N; i++) {
+			char c = line.charAt(i);
+
+			if (c == 'U')
+				seaLevel++;
+			else
+				seaLevel--;
+
+			if (last < 0 && seaLevel == 0)
+				count++;
+
+			last = seaLevel;
+		}
+
+		pw.println(count);
+		line = null;
+		br.close();
+		br = null;
+		pw.close();
+		pw = null;
+		System.gc();
+	}
+}
