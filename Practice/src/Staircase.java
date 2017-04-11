1d3bfa8c6be8e836b1642dbbc0bdc88359e7feea
Lakshmikant Anandkumar Deshpande <lakshmikantdeshpande@gmail.com>
2017-04-11 14:14:42 +0530

    Divisible Sum Pairs and Staircase solved

---
+++ Practice/src/Staircase.java
@@ -0,0 +1,27 @@
+import java.io.PrintWriter;
+import java.util.Scanner;
+
+public class Staircase {
+
+	static Scanner scanner = new Scanner(System.in);
+	static PrintWriter pw = new PrintWriter(System.out);
+
+	public static void main(String args[]) throws Exception {
+		int N = scanner.nextInt();
+		int spaces = N;
+
+		while (spaces-- > 0) {
+			for (int i = 0; i < spaces; i++)
+				pw.print(" ");
+			for (int i = 0; i < N - spaces; i++)
+				pw.print("#");
+			pw.println();
+		}
+
+		pw.close();
+		pw = null;
+		scanner.close();
+		scanner = null;
+		System.gc();
+	}
+}
