da3978f0c620a4b573856baaf9ee8b0fb28a498b
Lakshmikant Anandkumar Deshpande <lakshmikantdeshpande@gmail.com>
2017-04-13 00:33:39 +0530

    2 Simple codes for timepass

---
+++ Practice/src/Library_Fine.java
@@ -0,0 +1,39 @@
+import java.io.PrintWriter;
+import java.util.Scanner;
+
+public class Library_Fine {
+
+	public static void main(String args[]) throws Exception {
+		Scanner scanner = new Scanner(System.in);
+		PrintWriter pw = new PrintWriter(System.out);
+
+		int actualDay = scanner.nextInt();
+		int actualMonth = scanner.nextInt();
+		int actualYear = scanner.nextInt();
+
+		int expectedDay = scanner.nextInt();
+		int expectedMonth = scanner.nextInt();
+		int expectedYear = scanner.nextInt();
+
+		if (actualYear < expectedYear || ((actualYear == expectedYear) && (actualMonth < expectedMonth))
+				|| ((actualYear == expectedYear) && (actualMonth == expectedMonth) && (actualDay <= expectedDay)))
+			pw.println(0);
+		else {
+			int fine = 0;
+
+			if (actualYear > expectedYear)
+				fine += 10000;
+			else if (actualMonth > expectedMonth)
+				fine += 500 * (actualMonth - expectedMonth);
+			else
+				fine += (actualDay - expectedDay) * 15;
+			pw.println(fine);
+		}
+
+		pw.close();
+		pw = null;
+		scanner.close();
+		scanner = null;
+		System.gc();
+	}
+}
---
+++ Practice/src/Save_the_Prisoner.java
@@ -0,0 +1,25 @@
+import java.io.PrintWriter;
+import java.util.Scanner;
+
+public class Save_the_Prisoner {
+
+	public static void main(String args[]) throws Exception {
+		Scanner scanner = new Scanner(System.in);
+		PrintWriter pw = new PrintWriter(System.out);
+
+		int T = scanner.nextInt();
+
+		while (T-- > 0) {
+			long prisoners = scanner.nextLong();
+			long sweets = scanner.nextLong();
+			long start = scanner.nextLong();
+			pw.println(((sweets + start - 1) % prisoners) == 0 ? prisoners : ((sweets + start - 1) % prisoners));
+		}
+
+		pw.close();
+		pw = null;
+		scanner.close();
+		scanner = null;
+		System.gc();
+	}
+}
