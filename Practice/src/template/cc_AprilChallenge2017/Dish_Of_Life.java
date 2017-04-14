828aa732c2f10f1bc736cf350886d308055d9ea8
Lakshmikant Anandkumar Deshpande <lakshmikantdeshpande@gmail.com>
2017-04-14 23:50:03 +0530

    Codechef April Challenge

---
+++ Practice/src/template/cc_AprilChallenge2017/Dish_Of_Life.java
@@ -0,0 +1,59 @@
+package template.cc_AprilChallenge2017;
+
+import java.io.PrintWriter;
+import java.util.HashSet;
+import java.util.Scanner;
+import java.util.Set;
+
+class Dish_Of_Life {
+
+	public static void main(String args[]) throws Exception {
+		Scanner scanner = new Scanner(System.in);
+		PrintWriter pw = new PrintWriter(System.out);
+
+		int N = scanner.nextInt();
+
+		while (N-- > 0) {
+			int islands = scanner.nextInt();
+			int ingredients = scanner.nextInt();
+			Set<Integer> set = new HashSet<Integer>();
+			short status = -1;
+
+			// 0 sad
+			// 1 some
+			// 2 all
+
+			for (int i = 0; i < islands; i++) {
+				int island = scanner.nextInt();
+				while (island-- > 0) {
+					int temp = scanner.nextInt();
+					if (temp <= ingredients)
+						set.add(temp);
+				}
+
+				if (status == -1) {
+					if (set.size() == ingredients && i == 0)
+						if (islands == 1)
+							status = 2;
+						else
+							status = 1;
+					else if (set.size() == ingredients && i < islands - 1)
+						status = 1;
+					else if (set.size() == ingredients && i == islands - 1)
+						status = 2;
+				}
+			}
+
+			if (status == -1)
+				pw.println("sad");
+			else
+				pw.println(status == 1 ? "some" : "all");
+		}
+
+		pw.close();
+		pw = null;
+		scanner.close();
+		scanner = null;
+		System.gc();
+	}
+}
