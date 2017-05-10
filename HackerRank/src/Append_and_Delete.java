600516e7fdc4b8eafc90fd79e4362a54e568201e
Lakshmikant Deshpande <lakshmikantdeshpande@gmail.com>
2017-05-11 01:16:34 +0530

    Append and delete, Bitwise AND solved

---
+++ HackerRank/src/Append_and_Delete.java
@@ -0,0 +1,31 @@
+import java.io.PrintWriter;
+import java.util.Scanner;
+
+public class Append_and_Delete {
+
+    public static void main(String args[]) throws Exception {
+        Scanner scanner = new Scanner(System.in);
+        PrintWriter pw = new PrintWriter(System.out);
+
+        final String yes = "Yes";
+        final String no = "No";
+        String source = scanner.nextLine();
+        String destination = scanner.nextLine();
+        int k = scanner.nextInt();
+        int i = 0, difference = 0;
+
+        while (i < source.length() && i < destination.length() && source.charAt(i) == destination.charAt(i))
+            i++;
+        difference = source.length() + destination.length() - i * 2;  // as 2*i characters are covered
+        if ((difference <= k && difference % 2 == k % 2) || source.length() + destination.length() < k)
+            pw.println(yes);
+        else
+            pw.println(no);
+
+        pw.close();
+        pw = null;
+        scanner.close();
+        scanner = null;
+        System.gc();
+    }
+}
---
+++ HackerRank/src/Bitwise_AND.java
@@ -0,0 +1,39 @@
+import java.io.PrintWriter;
+import java.util.Scanner;
+
+public class Bitwise_AND {
+
+    public static void main(String args[]) throws Exception {
+        Scanner scanner = new Scanner(System.in);
+        PrintWriter pw = new PrintWriter(System.out);
+
+        int T = scanner.nextInt();
+        while (T-- > 0) {
+            int N = scanner.nextInt();
+            int max = scanner.nextInt();
+
+            int currMax = 0;
+            for (int i = 1; i <= N; i++) {
+                boolean flag = false;
+                for (int j = i + 1; j <= N; j++) {
+                    int t = i & j;
+                    if (t == max - 1) {
+                        currMax = t;
+                        flag = true;
+                        break;
+                    } else if (t > currMax && t < max)
+                        currMax = t;
+                }
+                if (flag)
+                    break;
+            }
+            pw.println(currMax);
+        }
+
+        pw.close();
+        pw = null;
+        scanner.close();
+        scanner = null;
+        System.gc();
+    }
+}
