a74701fc8ed9406e63854da76b4d01776809f64a
Lakshmikant Deshpande <lakshmikantdeshpande@gmail.com>
2017-05-19 23:15:37 +0530

    Learning Sieve of Eratosthenes

---
+++ SP/PRIME1.java
@@ -0,0 +1,14 @@
+import java.io.PrintWriter;
+import java.util.Scanner;
+
+public class PRIME1 {
+    public static void main(String args[]) throws Exception {
+        Scanner scanner = new Scanner(System.in);
+        PrintWriter pw = new PrintWriter(System.out);
+
+        int N = scanner.nextInt();
+
+        pw.close();
+        scanner.close();
+    }
+}
