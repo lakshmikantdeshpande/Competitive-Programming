abaa52b93bd2066176c47cd66a8c56cbb2913da4
Lakshmikant Deshpande <lakshmikantdeshpande@gmail.com>
2017-05-22 00:11:54 +0530

    Fair Rations (Improved after reading the editorial)

--- HackerRank/Fair_Rations.java
+++ HackerRank/Fair_Rations.java
@@ -1,5 +1,4 @@
-// Nasty code
-// will improve today
+// N time
 
 import java.io.PrintWriter;
 import java.util.Scanner;
@@ -10,40 +9,25 @@ class Fair_Rations {
         PrintWriter pw = new PrintWriter(System.out);
 
         int N = scanner.nextInt();
-        int loaves = 0;
-
         int[] array = new int[N];
         for (int i = 0; i < N; i++) {
             array[i] = scanner.nextInt();
         }
+        int loaves = 0;
 
-        if (N == 2) {
-            if ((isEven(array[0]) && !isEven(array[1])) || (!isEven(array[0]) && isEven(array[1]))) {
-                pw.println("NO");
-                pw.close();
-                return;
-            } else if ((isEven(array[0])) && (isEven(array[1])))
-                pw.println(0);
-            else
-                pw.println(1);
-        } else {
-            for (int i = 0; i < N - 1; i++) {
-                if (i == 0 && !isEven(array[i])) {
-                    array[i]++;
-                    array[i + 1]++;
-                    loaves += 2;
-                } else if (i == N - 1 && ((!isEven(i) && isEven(i + 1)) || (isEven(i) && !isEven(i + 1)))) {
-                    pw.println("NO");
-                    pw.close();
-                    return;
-                } else if ((!isEven(array[i]) && isEven(array[i + 1])) || (!isEven(array[i]) && !isEven(array[i + 1]))) {
-                    array[i]++;
-                    array[i + 1]++;
-                    loaves += 2;
-                }
+        for (int i = 0; i < N - 1; i++) {
+            if (!isEven(array[i])) {      // if it is an odd number
+                array[i]++;
+                array[i + 1]++;
+                loaves += 2;
             }
         }
-        pw.println(verify(array) ? loaves : "NO");
+
+        if (!isEven(array[N - 1]))
+            pw.println("NO");   // if last number is odd, print NO
+        else
+            pw.println(loaves);
+
         pw.close();
         scanner.close();
     }
@@ -51,12 +35,4 @@ class Fair_Rations {
     private static boolean isEven(int number) {
         return number % 2 == 0;
     }
-
-    private static boolean verify(int[] array) {
-        for (int i : array)
-            if (!isEven(i))
-                return false;
-        return true;
-    }
-
 }
