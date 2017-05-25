5f76237863c391f713c4f507b95c3665799dceae
Lakshmikant Deshpande <lakshmikantdeshpande@gmail.com>
2017-05-25 23:21:45 +0530

    Solved: Cavity Map The Grid Search (Simple Brute Force)

--- HackerRank/Cavity_Map.java
+++ HackerRank/Cavity_Map.java
@@ -1,4 +1,47 @@
-package PACKAGE_NAME;
+// N time N space
+
+import java.io.PrintWriter;
+import java.util.Scanner;
 
 public class Cavity_Map {
+    public static void main(String args[]) throws Exception {
+        Scanner scanner = new Scanner(System.in);
+        PrintWriter pw = new PrintWriter(System.out);
+
+        int N = scanner.nextInt();
+        scanner.nextLine();
+        int[][] array = new int[N][N];
+
+        for (int i = 0; i < N; i++) {
+            String row = scanner.nextLine();
+            for (int j = 0; j < N; j++) {
+                array[i][j] = Character.getNumericValue(row.charAt(j));
+            }
+        }
+
+        // if N <= 2, we don't need to check, as only border points will be there that can't be marked as X
+        if (N > 2) {
+            for (int i = 1; i < N - 1; i++) {
+                for (int j = 1; j < N - 1; j++) {
+                    int temp = array[i][j];
+                    // surrounding cells must be strictly smaller
+                    if (temp > array[i - 1][j] && temp > array[i + 1][j] && temp > array[i][j + 1] && temp > array[i][j - 1])
+                        array[i][j] = Integer.MAX_VALUE;
+                }
+            }
+        }
+        print(array, pw);
+
+        pw.close();
+        scanner.close();
+    }
+
+    private static void print(int[][] array, PrintWriter pw) {
+        for (int i = 0; i < array.length; i++) {
+            for (int j = 0; j < array.length; j++) {
+                pw.print(array[i][j] == Integer.MAX_VALUE ? "X" : array[i][j]);
+            }
+            pw.println();
+        }
+    }
 }
--- HackerRank/The_Grid_Search.java
+++ HackerRank/The_Grid_Search.java
@@ -1,4 +1,76 @@
-package PACKAGE_NAME;
+// n * n time 1 space
+
+import java.io.PrintWriter;
+import java.util.Scanner;
 
 public class The_Grid_Search {
+    public static void main(String args[]) throws Exception {
+        Scanner scanner = new Scanner(System.in);
+        PrintWriter pw = new PrintWriter(System.out);
+
+        int T = scanner.nextInt();
+        while (T-- > 0) {
+            int bWidth = scanner.nextInt();
+            int bHeight = scanner.nextInt();
+            scanner.nextLine();
+            char[][] bigArray = new char[bWidth][bHeight];
+
+            for (int i = 0; i < bWidth; i++) {
+                bigArray[i] = scanner.nextLine().toCharArray();
+            }
+
+            int sWidth = scanner.nextInt();
+            int sHeight = scanner.nextInt();
+            scanner.nextLine();
+
+            char[][] smallArray = new char[sWidth][sHeight];
+
+            for (int i = 0; i < sWidth; i++) {
+                smallArray[i] = scanner.nextLine().toCharArray();
+            }
+
+            if (sWidth > bWidth || sHeight > bHeight) {
+                pw.println("NO");
+            } else {
+                int diffWidth = bWidth - sWidth;
+                int diffHeight = bHeight - sHeight;
+                boolean flag = false;
+
+                for (int i = 0; i < diffWidth + 1; i++) {
+                    for (int j = 0; j < diffHeight + 1; j++) {
+                        if (bigArray[i][j] == smallArray[0][0]) {
+                            if (rectangleFound(bigArray, smallArray, i, j, sHeight, sWidth)) {
+                                pw.println("YES");
+                                flag = true;
+                                break;
+                            }
+                        }
+                    }
+                    if (flag)
+                        break;
+                }
+                if (!flag)
+                    pw.println("NO");
+            }
+        }
+
+        pw.close();
+        scanner.close();
+    }
+
+    // bigger array, smaller array, original i, original j, smaller array height and width
+    private static boolean rectangleFound(char[][] bigArray, char[][] smallArray, int oi, int oj, int sHeight, int sWidth) {
+        for (int i = oi; i < oi + sWidth; i++) {
+            boolean flag = false;
+            for (int j = oj; j < oj + sHeight; j++) {
+                if (bigArray[i][j] != smallArray[i - oi][j - oj]) {
+                    flag = true;
+                    break;
+                }
+            }
+            if (flag)
+                return false;
+        }
+        return true;
+    }
 }
