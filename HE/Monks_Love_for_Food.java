f77508d39e7ec71a45e09f920024f003ec949eea
Lakshmikant Deshpande <lakshmikantdeshpande@gmail.com>
2017-05-29 23:42:16 +0530

    HE Monk's love for food solved

---
+++ HE/Monks_Love_for_Food.java
@@ -0,0 +1,64 @@
+import java.io.PrintWriter;
+import java.util.Scanner;
+
+class Monks_Love_for_Food {
+    static class Stack {
+        private int top = -1;
+        private int size = 0;
+        private final int[] array;
+
+        Stack(int sz) {
+            if (sz <= 0 || sz == Integer.MAX_VALUE)
+                sz = 10;
+            top = -1;
+            size = sz;
+            array = new int[sz];
+        }
+
+        Stack() {
+            this(100);
+        }
+
+        public synchronized void push(int data) throws Exception {
+            if (top + 1 == size - 1)
+                throw new Exception("OVERFLOW");
+            array[++top] = data;
+        }
+
+        public synchronized int pop() throws Exception {
+            if (isEmpty())
+                throw new Exception("UNDERFLOW");
+            int retVal = array[top];
+            array[top--] = 0;
+            return retVal;
+        }
+
+        public boolean isEmpty() {
+            return top == -1;
+        }
+
+    }
+
+    public static void main(String[] args) throws Exception {
+        PrintWriter pw = new PrintWriter(System.out);
+        Scanner scanner = new Scanner(System.in);
+
+        int queries = scanner.nextInt();
+        Stack stack = new Stack(1024);
+
+        while (queries-- > 0) {
+            int choice = scanner.nextInt();
+            if (choice == 1) {
+                if (stack.isEmpty())
+                    pw.println("No Food");
+                else
+                    pw.println(stack.pop());
+            } else {
+                stack.push(scanner.nextInt());
+            }
+        }
+
+        pw.close();
+        scanner.close();
+    }
+}
