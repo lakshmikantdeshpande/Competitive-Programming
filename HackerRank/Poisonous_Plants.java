5573f8ae0464b8b82679c51dfa050e1c63a93633
Lakshmikant Deshpande <lakshmikantdeshpande@gmail.com>
2017-06-07 21:27:36 +0530

    Poisonous Plants Solved

--- HackerRank/Poisonous_Plants.java
+++ HackerRank/Poisonous_Plants.java
@@ -1,66 +1,45 @@
 // N time N space
 
-import java.io.BufferedReader;
-import java.io.InputStreamReader;
 import java.io.PrintWriter;
-import java.util.LinkedList;
-import java.util.List;
-import java.util.ListIterator;
-import java.util.StringTokenizer;
+import java.util.Scanner;
+import java.util.Stack;
 
 class Poisonous_Plants {
     public static void main(String args[]) throws Exception {
-        BufferedReader br = new BufferedReader(new InputStreamReader(System.in));
+        Scanner scanner = new Scanner(System.in);
         PrintWriter pw = new PrintWriter(System.out);
 
-        int N = parseInt(br.readLine());
-        List<Long> list = new LinkedList<>();
-
-        // initially we get the reverse stack, so we used stack.peek() >= temp instead of <=
-        StringTokenizer strt = new StringTokenizer(br.readLine());
-
-        for (int i = 0; i < N; i++) {
-            list.add(parseLong(strt.nextToken()));
+        int[] array = new int[scanner.nextInt()];
+        for (int i = 0; i < array.length; i++) {
+            array[i] = scanner.nextInt();
         }
 
-        if (N == 1)
-            pw.println(0);
-        else {
-            boolean isFinished = false;
-            int counter = 0;
+        Stack<Integer> stack = new Stack<>();
+        int i = array.length - 1;
+        boolean day1 = false;
 
-            while (!isFinished) {
-                boolean plantDied = false;
-                ListIterator<Long> iterator = list.listIterator();
-                long last = iterator.next();
-
-                while (iterator.hasNext()) {
-                    long temp = iterator.next();
-                    if (temp > last) {
-                        iterator.remove();
-                        plantDied = true;
-                    }
-                    last = temp;
-                }
-
-                // if no plants die, dying will be finished (isFinished = true)
-                if (!plantDied)
-                    isFinished = true;
-                else
-                    counter++;
+        int min = 0, max = 0, days = 0;
+        while (i >= 0) {
+            // find the rightmost index for removal
+            while (i > 0 && array[i] > array[i - 1]) {
+                i--;
+                day1 = true;
             }
-            pw.println(counter);
+
+            while (!stack.isEmpty() && array[i] < stack.peek()) {
+                max = Math.max(max, stack.size());
+                stack.pop();
+            }
+
+            min = Math.max(stack.size(), 0);
+            days = Math.max(days, max - min);
+            // push the new rightmost index
+            stack.push(array[i--]);
         }
+
+        pw.println(days + (day1 ? 1 : 0));
 
         pw.close();
-        br.close();
-    }
-
-    private static int parseInt(String string) {
-        return Integer.parseInt(string);
-    }
-
-    private static long parseLong(String string) {
-        return Long.parseLong(string);
+        scanner.close();
     }
 }
