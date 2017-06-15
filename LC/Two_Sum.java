fa4fb424847860c734bec1ddc77aaf58471776b1
Lakshmikant Deshpande <lakshmikantdeshpande@gmail.com>
2017-06-15 15:04:14 +0530

    Two Sum One more way

--- LC/Two_Sum.java
+++ LC/Two_Sum.java
@@ -1,4 +1,6 @@
 // N time N space
+// N + N time N space
+// N * N time 1 space
 
 import java.util.Arrays;
 import java.util.HashMap;
@@ -21,6 +23,22 @@ class Two_Sum {
 
     public int[] twoSum1(int[] nums, int target) {
         int[] answer = new int[2];
+        Map<Integer, Integer> map = new HashMap<>();
+        for (int i = 0; i < nums.length; i++) {
+            map.put(nums[i], i);
+        }
+
+        for (int i = 0; i < nums.length; i++) {
+            int diff = target - nums[i];
+            if (map.containsKey(diff) && i != map.get(diff)) {
+                return new int[]{i, map.get(diff)};
+            }
+        }
+        return answer;
+    }
+
+    public int[] twoSum2(int[] nums, int target) {
+        int[] answer = new int[2];
         for (int i = 0; i < nums.length; i++) {
             for (int j = i + 1; j < nums.length; j++) {
                 if (nums[i] + nums[j] == target)
@@ -35,5 +53,6 @@ class Two_Sum {
         int[] array = new int[]{2, 7, 11, 15};
         System.out.println(Arrays.toString(ts.twoSum(array, 9)));
         System.out.println(Arrays.toString(ts.twoSum1(array, 9)));
+        System.out.println(Arrays.toString(ts.twoSum2(array, 9)));
     }
 }
