9162e27f833c96d87fc7f24f34c9e3a1faf88c23
Lakshmikant Deshpande <lakshmikantdeshpande@gmail.com>
2017-06-16 16:38:54 +0530

    Fixed Reverse Integer

--- LC/Reverse_Integer.java
+++ LC/Reverse_Integer.java
@@ -1,10 +1,12 @@
 public class Reverse_Integer {
     public int reverse(int x) {
-        int temp = 0;
+        long temp = 0;
         while (x != 0) {
             temp = temp * 10 + x % 10;
             x /= 10;
+            if (temp > Integer.MAX_VALUE || temp < Integer.MIN_VALUE)
+                return 0;
         }
-        return temp;
+        return (int)temp;
     }
 }
