e458c17147fe1466835c8e60cf9411cebf1cdeb9
Lakshmikant Deshpande <lakshmikantdeshpande@gmail.com>
2017-07-02 01:03:52 +0530

    Super Reduced String Solved

--- HackerRank/Super_Reduced_String.java
+++ HackerRank/Super_Reduced_String.java
@@ -6,7 +6,15 @@ public class Super_Reduced_String {
         Scanner scanner = new Scanner(System.in);
         PrintWriter pw = new PrintWriter(System.out);
 
-        int N = scanner.nextInt();
+        int[] array = new int[26];
+
+        for (char c : scanner.nextLine().toCharArray())
+            ++array[c - 97];
+
+        for (int i = 0; i < 26; i++) {
+            if (array[i] % 2 != 0)
+                pw.print((char) (i + 97));
+        }
 
         pw.close();
         scanner.close();
