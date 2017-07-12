772677c569fb711689648feffc6221fb888c1bd2
Lakshmikant Deshpande <lakshmikantdeshpande@gmail.com>
2017-07-12 19:17:52 +0530

    Alternating Characters

--- HackerRank/Alternating_Characters.java
+++ HackerRank/Alternating_Characters.java
@@ -1,7 +1,34 @@
-package PACKAGE_NAME;
+// N time 1 space
 
-/**
- * Created by s on 15/7/17.
- */
+import java.io.PrintWriter;
+import java.util.Scanner;
+
 public class Alternating_Characters {
+    public static void main(String args[]) throws Exception {
+        Scanner scanner = new Scanner(System.in);
+        PrintWriter pw = new PrintWriter(System.out);
+
+        int N = scanner.nextInt();
+        scanner.nextLine();
+        while (N-- > 0) {
+            pw.println(solve(scanner.nextLine()));
+        }
+
+        pw.close();
+        scanner.close();
+    }
+
+    private static int solve(String s) {
+        int counter = 0;
+        if (s.length() < 2)
+            return 1;
+        char last = s.charAt(0);
+        for (int i = 1; i < s.length(); i++) {
+            if (s.charAt(i) == last)
+                counter++;
+            else
+                last = s.charAt(i);
+        }
+        return counter;
+    }
 }
