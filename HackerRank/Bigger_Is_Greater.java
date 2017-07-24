d16c67c7e1b85b1e92d1a0b814d48eab09436845
Lakshmikant Deshpande <lakshmikantdeshpande@gmail.com>
2017-07-24 22:12:40 +0530

    Bigger is greater

--- HackerRank/Bigger_Is_Greater.java
+++ HackerRank/Bigger_Is_Greater.java
@@ -1,12 +1,8 @@
-// TODO
-
 import java.io.PrintWriter;
-import java.util.ArrayList;
-import java.util.Collections;
-import java.util.List;
+import java.util.Arrays;
 import java.util.Scanner;
 
-public class Bigger_Is_Greater {
+class Bigger_Is_Greater {
     public static void main(String args[]) throws Exception {
         Scanner scanner = new Scanner(System.in);
         PrintWriter pw = new PrintWriter(System.out);
@@ -14,70 +10,32 @@ public class Bigger_Is_Greater {
         int N = scanner.nextInt();
         scanner.nextLine();
         while (N-- > 0) {
-            String string = scanner.nextLine();
-            List<Pair> list = new ArrayList<>();
+            char array[] = scanner.nextLine().toCharArray();
+            int i = array.length - 1;
+            while (i > 0 && array[i - 1] >= array[i])
+                i--;
 
-            for (int i = 0; i < string.length(); i++) {
-                char c = string.charAt(i);
-                list.add(new Pair(c, i));
-            }
-            Collections.sort(list); // sorted by characters
-            if (list.get(0).character == list.get(list.size() - 1).character) {
+            if (i <= 0) {
                 pw.println("no answer");
                 continue;
             }
 
-            int i = 0;
-            Pair pair = list.get(0);
-            char last = (char) pair.character;
-            for (i = 1; i < list.size(); i++) {
-                Pair pa = list.get(i);
-                if (pa.character > last && pa.index > pair.index) {
-                    pw.println(swap(string, pair.index, pa.index));
-                }
-            }
+            int j = array.length - 1;
+            while (array[j] <= array[i - 1])
+                j--;
+
+            char temp = array[i - 1];
+            array[i - 1] = array[j];
+            array[j] = temp;
+
+            Arrays.sort(array, i, array.length);
+
+            for (int a : array)
+                pw.print(a);
+            pw.println();
         }
+
         pw.close();
         scanner.close();
-    }
-
-    private static String swap(String temp, int i, int j) {
-        StringBuilder strb = new StringBuilder();
-        for (int k = 0; k < temp.length(); k++) {
-            if (k == i)
-                strb.append(temp.charAt(j));
-            else if (k == j)
-                strb.append(temp.charAt(i));
-            else
-                strb.append(temp.charAt(k));
-        }
-        return strb.toString();
-    }
-
-    private static class Pair implements Comparable {
-        int character;
-        int index;
-
-        public Pair(int character, int index) {
-            this.character = character;
-            this.index = index;
-        }
-
-        @Override
-        public int compareTo(Object o) {
-            if (this.character < ((Pair) o).character)
-                return -1;
-            else if (this.character > ((Pair) o).character)
-                return 1;
-            return 0;
-        }
-
-        @Override
-        public String toString() {
-            return "Pair{" +
-                    "character=" + character +
-                    ", index=" + index +
-                    '}';
-        }
     }
 }
