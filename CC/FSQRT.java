500b6b8c50822c0ede881dddb21522e1fc1f35d9
Lakshmikant Deshpande <lakshmikantdeshpande@gmail.com>
2017-08-21 14:59:43 +0530

    FSQRT Solved

---
+++ CC/FSQRT.java
@@ -0,0 +1,18 @@
+import java.io.PrintWriter;
+import java.util.Scanner;
+
+class FSQRT {
+    public static void main(String args[]) throws Exception {
+        Scanner scanner = new Scanner(System.in);
+        PrintWriter pw = new PrintWriter(System.out);
+
+        int N = scanner.nextInt();
+        while (N-- > 0) {
+            int temp = scanner.nextInt();
+            pw.println(Math.round(Math.sqrt(temp)));
+        }
+
+        pw.close();
+        scanner.close();
+    }
+}
