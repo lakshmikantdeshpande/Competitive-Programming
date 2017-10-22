bb5a0f52a4e336850e6100af97c9593c86bb398a
Lakshmikant Anandkumar Deshpande <lakshmikantdeshpande@gmail.com>
2017-10-22 18:09:41 +0530

    Lonely Integer

---
+++ HackerRank/Lonely_Integer.java
@@ -0,0 +1,21 @@
+// N time 1 space
+
+import java.io.PrintWriter;
+import java.util.Scanner;
+
+class Lonely_Integer {
+    public static void main(String args[]) throws Exception {
+        Scanner scanner = new Scanner(System.in);
+        PrintWriter pw = new PrintWriter(System.out);
+
+        int N = scanner.nextInt();
+        int temp = 0;
+
+        while (N-- > 0)
+            temp = temp ^ scanner.nextInt();
+        pw.println(temp);
+
+        pw.close();
+        scanner.close();
+    }
+}
