78ceb2881cf0ecedc95af309a46e1f7c555b3542
Lakshmikant Deshpande <lakshmikantdeshpande@gmail.com>
2017-10-25 08:28:23 +0530

    Added a corner case

--- GFG/Array_Rotation1.java
+++ GFG/Array_Rotation1.java
@@ -1,4 +1,4 @@
-// N time N space
+// N time D space
 
 import com.sun.istack.internal.NotNull;
 
@@ -6,25 +6,27 @@ import java.util.Arrays;
 
 public class Array_Rotation1 {
 
-    private static int[] rotate(@NotNull int[] array, int shift) {
-        if (shift < 0 || shift > array.length) {
+    private static int[] rotate(@NotNull int[] array, final int shift) {
+        if (shift < 0) {
             throw new UnsupportedOperationException();
         } else if (shift == 0 || array.length == 0 || shift == array.length) {
             return array;
         }
 
-        int[] temp = new int[shift];
-        for (int i = 0; i < shift; i++) {
+        int hifts = shift % array.length;
+
+        int[] temp = new int[hifts];
+        for (int i = 0; i < hifts; i++) {
             temp[i] = array[i];
         }
 
         int t = 0;
-        for (int i = shift; i < array.length; i++) {
+        for (int i = hifts; i < array.length; i++) {
             array[t++] = array[i];
         }
 
         int p = 0;
-        for (int i = array.length - shift; i < array.length; i++) {
+        for (int i = array.length - hifts; i < array.length; i++) {
             array[i] = temp[p++];
         }
         return array;
@@ -34,5 +36,7 @@ public class Array_Rotation1 {
         int[] array = new int[]{1, 2, 3, 4, 5, 6, 7};
         int shift = 2;
         System.out.println(Arrays.toString(rotate(array, shift)));
+        array = new int[]{1, 2, 3, 4, 5, 6, 7};
+        System.out.println(Arrays.toString(rotate(array, 12)));
     }
 }
