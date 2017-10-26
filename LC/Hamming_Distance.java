67463739592620806b9f6eed0e5c99384790e6ee
Lakshmikant Deshpande <lakshmikantdeshpande@gmail.com>
2017-10-26 21:28:01 +0530

    Hamming distance Solved

---
+++ LC/Hamming_Distance.java
@@ -0,0 +1,14 @@
+// N time 1 Space
+class Hamming_Distance {
+    public int hammingDistance(int x, int y) {
+        return (countSetBits(x ^ y));
+    }
+
+    private int countSetBits(int num) {
+        int c = 0;
+        for (c = 0; num > 0; num = num & (num - 1)) {
+            ++c;
+        }
+        return c;
+    }
+}
