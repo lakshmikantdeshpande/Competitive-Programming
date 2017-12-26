abb4349219682250cf39df864c3f326811d45362
Lakshmikant Deshpande <lakshmikantdeshpande@gmail.com>
2017-12-26 08:09:53 +0530

    Makes10 and ParrotTrouble solved

--- CB/Makes10.java
+++ CB/Makes10.java
@@ -1,4 +1,7 @@
-package PACKAGE_NAME;
-
 public class Makes10 {
+    public boolean makes10(int a, int b) {
+        if (a == 10 || b == 10 || (a + b) == 10)
+            return true;
+        return false;
+    }
 }
--- CB/ParrotTrouble.java
+++ CB/ParrotTrouble.java
@@ -1,4 +1,8 @@
-package PACKAGE_NAME;
-
 public class ParrotTrouble {
+    public boolean parrotTrouble(boolean talking, int hour) {
+        if (talking && (hour > 20 || hour < 7))
+            return true;
+        else
+            return false;
+    }
 }
