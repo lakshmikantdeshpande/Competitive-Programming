581f10d3459ea5a0cc1ce20dfaa9ee1492ee8d9d
Lakshmikant Deshpande <lakshmikantdeshpande@gmail.com>
2018-01-03 21:58:54 +0530

    Moved legacy code, started HackerRank fresh

---
+++ hackerrank/ArraysDs.java
@@ -0,0 +1,24 @@
+import java.util.Scanner;
+import java.util.Stack;
+import java.io.PrintWriter;
+
+public class ArraysDs {
+
+    public static void main(String[] args) {
+        Stack<Integer> stack = new Stack<>();
+
+        Scanner scanner = new Scanner(System.in);
+        int T = scanner.nextInt();
+        while (T-- > 0) {
+            stack.push(scanner.nextInt());
+        }
+        scanner.close();
+
+        PrintWriter printWriter = new PrintWriter(System.out);
+        while (!stack.isEmpty()) {
+            printWriter.print(stack.pop() + " ");
+        }
+        printWriter.close();
+    }
+
+}
