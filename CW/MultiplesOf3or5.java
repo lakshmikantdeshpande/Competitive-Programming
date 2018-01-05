bc31df91ce6a52f97282cbdd8b4c8a5ef395bd71
Lakshmikant Deshpande <lakshmikantdeshpande@gmail.com>
2018-01-05 23:06:00 +0530

    Codewars first problem

---
+++ CW/MultiplesOf3or5.java
@@ -0,0 +1,11 @@
+public class MultiplesOf3or5 {
+    public int solution(int number) {
+        int sum = 0;
+        for (int i = 1; i < number; i++) {
+            if (i % 3 == 0 || i % 5 == 0) {
+                sum += i;
+            }
+        }
+        return sum;
+    }
+}
