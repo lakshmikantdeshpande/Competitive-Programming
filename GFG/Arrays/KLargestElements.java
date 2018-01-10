d4927d4ce4b6b6d73e6c5f435236c31cb05c3d34
Lakshmikant Deshpande <lakshmikantdeshpande@gmail.com>
2018-01-10 20:44:36 +0530

    K largest elements solved

---
+++ GFG/Arrays/KLargestElements.java
@@ -0,0 +1,30 @@
+package Arrays;
+
+import java.io.PrintWriter;
+import java.util.Collections;
+import java.util.PriorityQueue;
+import java.util.Queue;
+import java.util.Scanner;
+
+class KLargestElements {
+    public static void main(String[] args) {
+        Queue<Integer> queue = new PriorityQueue<>(Collections.reverseOrder());
+        Scanner scanner = new Scanner(System.in);
+        PrintWriter printWriter = new PrintWriter(System.out);
+        int T = scanner.nextInt();
+        while (T-- > 0) {
+            int N = scanner.nextInt();
+            int noOfLargestElements = scanner.nextInt();
+            while (N-- > 0) {
+                queue.offer(scanner.nextInt());
+            }
+            while (noOfLargestElements-- > 0) {
+                printWriter.print(queue.poll() + " ");
+            }
+            queue.clear();
+            printWriter.print("\n");
+        }
+        printWriter.close();
+        scanner.close();
+    }
+}
