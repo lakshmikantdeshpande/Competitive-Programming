e7468a23ad1c0b532ac63b79d7c71cb9a20e479c
Lakshmikant Deshpande <lakshmikantdeshpande@gmail.com>
2019-03-20 20:29:10 +0530

    Created template

---
+++ Template.java
@@ -0,0 +1,14 @@
+import java.io.PrintWriter;
+import java.util.Scanner;
+
+class Solution {
+	public static void main(String[] args) {
+		Scanner scanner = new Scanner(System.in);
+		PrintWriter writer = new PrintWriter(System.out);
+
+		int T = scanner.nextInt();
+		
+		scanner.close();
+		writer.close();
+	}
+}
