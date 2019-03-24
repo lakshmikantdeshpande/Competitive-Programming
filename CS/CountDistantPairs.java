1a06910010441dc88ce5dbe7f621f52917d7978c
Lakshmikant Deshpande <lakshmikantdeshpande@gmail.com>
2019-03-24 21:29:19 +0530

    Count distant pairs

---
+++ CS/CountDistantPairs.java
@@ -0,0 +1,11 @@
+class Solution {
+    int countDistantPairs(String inputString, int distance) {
+        int count = 0;
+        for (int i = 0; i < inputString.length() - distance - 1; i++) {
+            if (inputString.charAt(i) == inputString.charAt(i + distance + 1)) {
+                count++;
+            }
+        }
+        return count;
+    }
+}
