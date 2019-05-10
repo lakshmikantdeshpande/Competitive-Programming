0598c01a96f62eba707efde79425a17fd2a9f7f9
Lakshmikant Deshpande <lakshmikantdeshpande@gmail.com>
2019-05-10 22:58:36 +0530

    Solved rotate array

---
+++ GFG/Arrays/RotateArray.java
@@ -0,0 +1,32 @@
+package GFG.Arrays;
+
+import java.io.PrintWriter;
+import java.util.Scanner;
+
+class RotateArray {
+  public static void main(String[] args) {
+    Scanner scanner = new Scanner(System.in);
+    PrintWriter writer = new PrintWriter(System.out);
+
+    int T = scanner.nextInt();
+    while (T-- > 0) {
+      int N = scanner.nextInt();
+      int D = scanner.nextInt();
+
+      int[] array = new int[N];
+      for (int i = 0; i < N; i++) {
+        array[i] = scanner.nextInt();
+      }
+
+
+      for (int i = D % N, count = 0; count < N; count++) {
+        writer.print(array[i] + " ");
+        i = (i + 1) % N;
+      }
+      writer.println();
+    }
+
+    scanner.close();
+    writer.close();
+  }
+}
