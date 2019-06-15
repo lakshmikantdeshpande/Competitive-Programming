60b4aeafbbc347e2de07e0757cbfdaa057dd2c2c
Lakshmikant Deshpande <lakshmikantdeshpande@gmail.com>
2019-06-15 05:55:45 +0530

    Solved BFS

--- GFG/Graphs/BFS.java
+++ GFG/Graphs/BFS.java
@@ -1,9 +1,11 @@
 package GFG.Graphs;
 
 import java.util.ArrayList;
+import java.util.LinkedList;
+import java.util.Queue;
 import java.util.Scanner;
 
-public class DFS {
+public class BFS {
   public static void main(String[] args) {
     Scanner sc = new Scanner(System.in);
     int t = sc.nextInt();
@@ -11,6 +13,7 @@ public class DFS {
       ArrayList<ArrayList<Integer>> list = new ArrayList<>();
       int vertices = sc.nextInt();
       int edges = sc.nextInt();
+
       for (int i = 0; i < vertices; i++)
         list.add(i, new ArrayList<>());
 
@@ -18,24 +21,30 @@ public class DFS {
         int u = sc.nextInt();
         int v = sc.nextInt();
         list.get(u).add(v);
-        list.get(v).add(u);
       }
       boolean[] vis = new boolean[vertices];
       for (int i = 0; i < vertices; i++)
         vis[i] = false;
-      Traversal.dfs(0, list, vis);
+      Traversal.bfs(0, list, vis);
       System.out.println();
     }
   }
 
   private static class Traversal {
-    static void dfs(int src, ArrayList<ArrayList<Integer>> graph, boolean[] visited) {
+    static void bfs(int src, ArrayList<ArrayList<Integer>> graph, boolean[] visited) {
+      Queue<Integer> queue = new LinkedList<>();
+      queue.offer(src);
       visited[src] = true;
-      System.out.print(src + " ");
 
-      for (int i : graph.get(src)) {
-        if (!visited[i]) {
-          dfs(i, graph, visited);
+      while (!queue.isEmpty()) {
+        int vertex = queue.poll();
+        System.out.print(vertex + " ");
+        ArrayList<Integer> adjacencyList = graph.get(vertex);
+        for (int v : adjacencyList) {
+          if (!visited[v]) {
+            visited[v] = true;
+            queue.add(v);
+          }
         }
       }
     }
