efab83a4c954fa1a9b7215a397822d369f39f8d0
Lakshmikant Deshpande <lakshmikantdeshpande@gmail.com>
2019-06-17 21:17:02 +0530

    Solved longest substring without repetitions

--- LC/LongestSubstringWithoutRepetitions.java
+++ LC/LongestSubstringWithoutRepetitions.java
@@ -5,8 +5,27 @@ import java.util.Set;
 
 public class LongestSubstringWithoutRepetitions {
 
+  private static int lengthOfLongestSubstringOptimal(String str) {
+    int max = 0;
+    int i = 0;
+    int j = 0;
+    int n = str.length();
+
+    Set<Character> set = new HashSet<>();
+    while (i < n && j < n) {
+      char c = str.charAt(j);
+      if (set.contains(c)) {
+        set.remove(str.charAt(i++));
+      } else {
+        set.add(str.charAt(j++));
+        max = Math.max(max, j - i);
+      }
+    }
+    return max;
+  }
+
   // Brute force
-  private int lengthOfLongestSubstringBrute(String str) {
+  private static int lengthOfLongestSubstringBrute(String str) {
     int maxLength = 0;
     int n = str.length();
 
@@ -21,7 +40,7 @@ public class LongestSubstringWithoutRepetitions {
     return maxLength;
   }
 
-  private boolean areAllUnique(String str, int i, int j) {
+  private static boolean areAllUnique(String str, int i, int j) {
     Set<Character> visited = new HashSet<>();
     for (; i < j; i++) {
       boolean added = visited.add(str.charAt(i));
@@ -33,7 +52,8 @@ public class LongestSubstringWithoutRepetitions {
   }
 
   public static void main(String[] args) {
-    System.out.println(new LongestSubstringWithoutRepetitions().lengthOfLongestSubstringBrute("aaababababaa"));
+    System.out.println(lengthOfLongestSubstringBrute("aaababababaa"));
+    System.out.println(lengthOfLongestSubstringOptimal("aaababababaa"));
   }
 }
 
