4f7f1417bc7b40935ba33dda13ffab418bb60e72
Lakshmikant Deshpande <lakshmikantdeshpande@gmail.com>
2019-06-23 22:23:08 +0530

    Solved Valid Parentheses

--- leetcode/ValidParentheses.java
+++ leetcode/ValidParentheses.java
@@ -1,24 +1,22 @@
-import java.util.Stack;
+package leetcode;
 
-class ValidParenthes {
+import java.util.ArrayDeque;
+import java.util.Deque;
+
+class ValidParentheses {
     public boolean isValid(String s) {
-        if (s == null) {
-            return false;
-        } else if (s.isEmpty()) {
-            return true;
-        }
-
-        Stack<Character> stack = new Stack<>();
+        if (s.isEmpty()) return true;
+        Deque<Character> stack = new ArrayDeque<>();
 
         for (char c : s.toCharArray()) {
-            if (stack.isEmpty()) {
+            if (c == '(' || c == '[' || c == '{') {
                 stack.push(c);
-            } else if ((c == '(' && stack.peek() == ')') || (c == ')' && stack.peek() == '(') ||
-                    (c == '{' && stack.peek() == '}') || (c == '}' && stack.peek() == '{') ||
-                    (c == '[' && stack.peek() == ']') || (c == ']' && stack.peek() == '[')) {
-                stack.pop();
             } else {
-                stack.push(c);
+                if (stack.isEmpty()) return false;
+                if (stack.peek() == '(' && c == ')') stack.pop();
+                else if (stack.peek() == '[' && c == ']') stack.pop();
+                else if (stack.peek() == '{' && c == '}') stack.pop();
+                else return false;
             }
         }
         return stack.isEmpty();
