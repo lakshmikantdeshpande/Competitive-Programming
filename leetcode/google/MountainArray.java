26b60027fb9c1219365927b7f87dfa6ce562f2d3
Lakshmikant Deshpande <lakshmikantdeshpande@gmail.com>
2019-07-07 19:30:49 +0530

    Solved Mountain Array

---
+++ leetcode/google/MountainArray.java
@@ -0,0 +1,22 @@
+package leetcode.google;
+
+class MountainArray {
+    public boolean validMountainArray(int[] A) {
+        if (A.length <= 2) return false;
+
+        int i = 0;
+        while (i < A.length - 1 && A[i] < A[i + 1]) {
+            i++;
+        }
+
+        if (i == 0 || i == A.length - 1) {
+            return false;
+        }
+
+        while (i < A.length - 1 && A[i] > A[i + 1]) {
+            i++;
+        }
+
+        return i == A.length - 1;
+    }
+}
