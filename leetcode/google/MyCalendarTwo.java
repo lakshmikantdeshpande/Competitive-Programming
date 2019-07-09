005f81004f6ed810952d9a2f51544f72622a747a
Lakshmikant Deshpande <lakshmikantdeshpande@gmail.com>
2019-07-09 14:30:50 +0530

    Solved MyCalendarTwo

---
+++ leetcode/google/MyCalendarTwo.java
@@ -0,0 +1,29 @@
+package leetcode.google;
+
+import java.util.TreeMap;
+
+class MyCalendarTwo {
+    private TreeMap<Integer, Integer> delta;
+
+    public MyCalendarTwo() {
+        delta = new TreeMap<>();
+    }
+
+    public boolean book(int start, int end) {
+        delta.put(start, delta.getOrDefault(start, 0) + 1);
+        delta.put(end, delta.getOrDefault(end, 0) - 1);
+
+        int active = 0;
+        for (int d : delta.values()) {
+            active += d;
+            if (active >= 3) {
+                delta.put(start, delta.get(start) - 1);
+                delta.put(end, delta.get(end) + 1);
+                if (delta.get(start) == 0)
+                    delta.remove(start);
+                return false;
+            }
+        }
+        return true;
+    }
+}
