6642f08fb7fe1ee010cd48456f4446b5021eff3a
Lakshmikant Deshpande <lakshmikantdeshpande@gmail.com>
2019-07-16 23:48:39 +0530

    Solved binary tree level order traversal

--- leetcode/google/BinaryTreeLevelOrderTraversal.java
+++ leetcode/google/BinaryTreeLevelOrderTraversal.java
@@ -16,38 +16,36 @@ public class BinaryTreeLevelOrderTraversal {
         }
     }
 
-    class Solution {
-        public List<List<Integer>> levelOrder(TreeNode root) {
-            List<List<Integer>> list = new ArrayList<>();
-            if (root == null) {
-                return list;
-            }
-
-            Queue<TreeNode> queue = new LinkedList<>();
-            queue.offer(root);
-            queue.offer(null);
-
-            while (!queue.isEmpty()) {
-                TreeNode node = queue.poll();
-                if (node == null) {
-                    if (!queue.isEmpty()) {
-                        queue.offer(null);
-                        list.add(new ArrayList<>());
-                    }
-                } else {
-                    if (list.isEmpty()) {
-                        list.add(new ArrayList<>());
-                    }
-                    list.get(list.size() - 1).add(node.val);
-                    if (node.left != null) {
-                        queue.offer(node.left);
-                    }
-                    if (node.right != null) {
-                        queue.offer(node.right);
-                    }
-                }
-            }
+    public List<List<Integer>> levelOrder(TreeNode root) {
+        List<List<Integer>> list = new ArrayList<>();
+        if (root == null) {
             return list;
         }
+
+        Queue<TreeNode> queue = new LinkedList<>();
+        queue.offer(root);
+        queue.offer(null);
+
+        while (!queue.isEmpty()) {
+            TreeNode node = queue.poll();
+            if (node == null) {
+                if (!queue.isEmpty()) {
+                    queue.offer(null);
+                    list.add(new ArrayList<>());
+                }
+            } else {
+                if (list.isEmpty()) {
+                    list.add(new ArrayList<>());
+                }
+                list.get(list.size() - 1).add(node.val);
+                if (node.left != null) {
+                    queue.offer(node.left);
+                }
+                if (node.right != null) {
+                    queue.offer(node.right);
+                }
+            }
+        }
+        return list;
     }
 }
