e7c67a841fb716bb223658ea531dfa374e81951c
Lakshmikant Deshpande <lakshmikantdeshpande@gmail.com>
2019-07-17 22:56:39 +0530

    Solved same tree

---
+++ leetcode/google/SameTree.java
@@ -0,0 +1,17 @@
+package leetcode.google;
+
+public class SameTree {
+
+    private static class TreeNode {
+        TreeNode left;
+        TreeNode right;
+        int val;
+    }
+
+    private boolean isSameTree(TreeNode p, TreeNode q) {
+        if (p == null && q == null) return true;
+        if (p == null || q == null) return false;
+
+        return p.val == q.val && isSameTree(p.left, q.left) && isSameTree(p.right, q.right);
+    }
+}
