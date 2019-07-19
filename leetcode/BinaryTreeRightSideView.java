4e4ca436f4355e44fe7418b130a7a9dbf8bb44cd
Lakshmikant Deshpande <lakshmikantdeshpande@gmail.com>
2019-07-19 09:47:22 +0530

    Solved binary tree right side view

---
+++ leetcode/BinaryTreeRightSideView.java
@@ -0,0 +1,40 @@
+package leetcode;
+
+import java.util.ArrayList;
+import java.util.LinkedList;
+import java.util.List;
+import java.util.Queue;
+
+class BinaryTreeRightSideView {
+
+    private static class TreeNode {
+        int val;
+        TreeNode left;
+        TreeNode right;
+    }
+
+    public List<Integer> rightSideView(TreeNode root) {
+        List<Integer> result = new ArrayList<>();
+        if (root == null) return result;
+
+        Queue<TreeNode> queue = new LinkedList<>();
+        queue.offer(root);
+        queue.offer(null);
+        TreeNode last = null;
+
+        while (!queue.isEmpty()) {
+            TreeNode node = queue.poll();
+            if (node == null) {
+                if (!queue.isEmpty()) {
+                    queue.offer(null);
+                }
+                result.add(last.val);
+            } else {
+                last = node;
+                if (node.left != null) queue.offer(node.left);
+                if (node.right != null) queue.offer(node.right);
+            }
+        }
+        return result;
+    }
+}
