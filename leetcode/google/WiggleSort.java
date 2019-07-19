a6aecc013dc3c70da16f02fb6dd881be60698643
Lakshmikant Deshpande <lakshmikantdeshpande@gmail.com>
2019-07-19 07:44:46 +0530

    Solved wiggle sort

---
+++ leetcode/google/WiggleSort.java
@@ -0,0 +1,19 @@
+package leetcode.google;
+
+public class WiggleSort {
+    public void wiggleSort(int[] nums) {
+        if (nums == null) return;
+
+        for (int i = 0; i < nums.length - 1; i++) {
+            if ((i % 2 == 0) == (nums[i] > nums[i + 1])) {
+                swap(nums, i, i + 1);
+            }
+        }
+    }
+
+    private void swap(int[] nums, int i, int j) {
+        int temp = nums[i];
+        nums[i] = nums[j];
+        nums[j] = temp;
+    }
+}
