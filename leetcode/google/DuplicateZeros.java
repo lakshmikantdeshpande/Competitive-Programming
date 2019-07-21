de7c51c8082be384b7f1e9c03767c74131833435
Lakshmikant Deshpande <lakshmikantdeshpande@gmail.com>
2019-07-21 12:19:02 +0530

    Simplified duplicate zeroes

--- leetcode/google/DuplicateZeros.java
+++ leetcode/google/DuplicateZeros.java
@@ -1,25 +1,39 @@
 package leetcode.google;
 
-class Solution {
+class DuplicateZeros {
+
     public void duplicateZeros(int[] arr) {
-        final int n = arr.length;
+        if (arr == null || arr.length == 0) return;
 
-        // count zeros
-        int count = 0;
-        for (int i = 0; i < n; i++) if (arr[i] == 0) count++;
-
-        // len is count of zeros + array length
-        int len = n + count;
-
-        // i starts and array length, j starts at array + offset length
-        for (int i = n - 1, j = len - 1; i >= 0 && j >= 0; i--, j--) {
-            if (arr[i] != 0) {
-                if (j < n) arr[j] = arr[i];
-            } else {
-                if (j < n) arr[j] = arr[i];
+        int zeroCount = getZeroCount(arr);
+        int i = arr.length - 1;
+        int j = arr.length - 1 + zeroCount;
+        while (i >= 0) {
+            if (arr[i] == 0) {
+                if (j < arr.length) {
+                    arr[j] = arr[i];
+                }
                 j--;
-                if (j < n) arr[j] = arr[i];
+                if (j < arr.length) {
+                    arr[j] = arr[i];
+                }
+                j--;
+                i--;
+            } else {
+                if (j < arr.length) {
+                    arr[j] = arr[i];
+                }
+                j--;
+                i--;
             }
         }
+    }
+
+    private int getZeroCount(int[] array) {
+        int count = 0;
+        for (int num : array) {
+            count += num == 0 ? 1 : 0;
+        }
+        return count;
     }
 }
