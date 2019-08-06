726057ad91069cc9d6ccbdff175396e158d4e768
Lakshmikant Deshpande <lakshmikantdeshpande@gmail.com>
2019-08-06 16:44:40 +0530

    Solved best time to buy and sell stock

--- leetcode/BestTimeToBuyAndSellStock.java
+++ leetcode/BestTimeToBuyAndSellStock.java
@@ -18,4 +18,16 @@ public class BestTimeToBuyAndSellStock {
         return max;
     }
 
+    private static int maxProfitTwo(int[] prices) {
+        int min = Integer.MAX_VALUE;
+        int maxProfit = 0;
+        for (int i = 0; i < prices.length; i++) {
+            if (prices[i] < min)
+                min = prices[i];
+            else if (prices[i] - min > maxProfit)
+                maxProfit = prices[i] - min;
+        }
+        return maxProfit;
+    }
+
 }
