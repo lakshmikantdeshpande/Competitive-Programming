e19aae83550cd2ff1ba4d35ec03ff4d2321fcc96
Lakshmikant Deshpande <lakshmikantdeshpande@gmail.com>
2019-08-14 23:18:47 +0530

    Solved two sum using hashmap

--- leetcode/amazon/TwoSum.java
+++ leetcode/amazon/TwoSum.java
@@ -1,5 +1,8 @@
 package leetcode.amazon;
 
+import java.util.HashMap;
+import java.util.Map;
+
 class TwoSum {
     public int[] bruteForce(int[] nums, int target) {
         for (int i = 0; i < nums.length; i++) {
@@ -13,4 +16,17 @@ class TwoSum {
         }
         return null;
     }
+
+    public int[] twoSum(int[] nums, int target) {
+        Map<Integer, Integer> map = new HashMap<>();
+        for (int i = 0; i < nums.length; i++) {
+            int num = nums[i];
+            if (map.containsKey(num)) {
+                return new int[]{map.get(num), i};
+            }
+            map.put(target - num, i);
+        }
+        return null;
+    }
+
 }
