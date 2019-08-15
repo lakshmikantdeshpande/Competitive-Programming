782b929d7f4bd423b6e6563b7be39b384757e657
Lakshmikant Deshpande <lakshmikantdeshpande@gmail.com>
2019-08-16 00:14:00 +0530

    Solved container with most water optimal

--- leetcode/amazon/ContainerWithMostWater.java
+++ leetcode/amazon/ContainerWithMostWater.java
@@ -10,4 +10,20 @@ public class ContainerWithMostWater {
         }
         return max;
     }
+
+    public int maxArea(int[] height) {
+        int i = 0;
+        int j = height.length - 1;
+        int max = 0;
+
+        while (i < j) {
+            max = Math.max(max, (j - i) * Math.min(height[i], height[j]));
+            if (height[i] <= height[j]) {
+                i++;
+            } else {
+                j--;
+            }
+        }
+        return max;
+    }
 }
