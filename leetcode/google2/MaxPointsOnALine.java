a3b1421fe7f8042912a56f906e464155fa766c98
lakshmikantdeshpande <lakshmikantdeshpande@gmail.com>
2021-04-20 09:04:41 +0530

    Solved max points on a line

--- leetcode/google2/MaxPointsOnALine.java
+++ leetcode/google2/MaxPointsOnALine.java
@@ -12,42 +12,44 @@ public class MaxPointsOnALine {
         }
 
         int globalMax = 0;
-
         for (int i = 0; i < points.length; i++) {
             int localMax = 0;
             int samePoint = 1;
-            Map<List<Integer>, Integer> slopeMap = new HashMap<>();
+            Map<List<Integer>, Integer> map = new HashMap<>();
 
             for (int j = i + 1; j < points.length; j++) {
-                int dx = points[i][0] - points[j][0];
-                int dy = points[i][1] - points[j][1];
+                int dy = points[j][1] - points[i][1];
+                int dx = points[j][0] - points[i][0];
 
                 if (dx == 0 && dy == 0) {
                     samePoint++;
                 } else {
-                    List<Integer> slope = getSlope(dy, dx);
-                    slopeMap.put(slope, slopeMap.getOrDefault(slope, 0) + 1);
-                    localMax = Math.max(localMax, slopeMap.get(slope));
+                    List<Integer> slope = getSlope(dx, dy);
+                    map.put(slope, map.getOrDefault(slope, 0) + 1);
+                    localMax = Math.max(localMax, map.get(slope));
                 }
             }
-
             globalMax = Math.max(globalMax, localMax + samePoint);
         }
         return globalMax;
     }
 
-    private List<Integer> getSlope(int dy, int dx) {
-        if (dx == 0) return Arrays.asList(1, 0);
-        if (dy == 0) return Arrays.asList(0, 1);
+    private List<Integer> getSlope(int x, int y) {
+        if (x == 0) {
+            return Arrays.asList(0, 1);
+        } else if (y == 0) {
+            return Arrays.asList(1, 0);
+        }
 
-        int d = gcd(dy, dx);
-        return Arrays.asList(dy / d, dx / d);
+        int gcd = gcd(x, y);
+        return Arrays.asList(x / gcd, y / gcd);
     }
 
-    private int gcd(int m, int n) {
-        if (n == 0) {
-            return m;
+    private int gcd(int x, int y) {
+        if (y == 0) {
+            return x;
         }
-        return gcd(n, m % n);
+
+        return gcd(y, x % y);
     }
 }
