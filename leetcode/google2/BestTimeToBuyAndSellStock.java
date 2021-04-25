d03237098cb4a800cc6641377ae76b2efd74d114
lakshmikantdeshpande <lakshmikantdeshpande@gmail.com>
2021-04-25 13:44:11 +0530

    https://leetcode.com/problems/best-time-to-buy-and-sell-stock/

---
+++ leetcode/google2/BestTimeToBuyAndSellStock.java
@@ -0,0 +1,19 @@
+public class BestTimeToBuyAndSellStock {
+    public int maxProfit(int[] prices) {
+        if (prices == null || prices.length < 2) {
+            return 0;
+        }
+
+        int min = Integer.MAX_VALUE;
+        int max = 0;
+
+        for (int price : prices) {
+            if (price < min) {
+                min = price;
+            } else if (price - min > max) {
+                max = price - min;
+            }
+        }
+        return max;
+    }
+}
