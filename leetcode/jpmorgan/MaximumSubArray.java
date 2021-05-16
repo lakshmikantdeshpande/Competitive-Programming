00d248a7d3b4e081d92a1b5d1a0a41909355fe05
Lakshmikant Deshpande <lakshmikantdeshpand@gmail.com>
2021-05-16 12:58:57 +0530

    https://leetcode.com/problems/maximum-subarray/

---
+++ leetcode/jpmorgan/MaximumSubArray.java
@@ -0,0 +1,14 @@
+package leetcode.jpmorgan;
+
+public class MaximumSubArray {
+    public int maxSubArray(int[] nums) {
+        int curr = nums[0];
+        int max = nums[0];
+
+        for (int i = 1; i < nums.length; i++) {
+            curr = Math.max(nums[i], curr + nums[i]);
+            max = Math.max(curr, max);
+        }
+        return max;
+    }
+}
