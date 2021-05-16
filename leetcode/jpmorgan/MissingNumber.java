fc825d374966c0fc3a1f56d3f7b7fcfe5a5c596f
Lakshmikant Deshpande <lakshmikantdeshpand@gmail.com>
2021-05-16 12:20:15 +0530

    https://leetcode.com/problems/missing-number/

---
+++ leetcode/jpmorgan/MissingNumber.java
@@ -0,0 +1,11 @@
+package leetcode.jpmorgan;
+
+public class MissingNumber {
+    public int missingNumber(int[] nums) {
+        int sum = 0;
+        for (int num : nums) {
+            sum += num;
+        }
+        return ((nums.length * (nums.length + 1)) / 2) - sum;
+    }
+}
