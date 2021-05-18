b6a9da5e9652af71bc309aaa9d474130aa7b8ad5
Lakshmikant Deshpande <lakshmikantdeshpand@gmail.com>
2021-05-18 22:54:36 +0530

    https://leetcode.com/problems/lowest-common-ancestor-of-a-binary-tree/

---
+++ leetcode/jpmorgan/LowestCommonAncestorOfABinaryTree.java
@@ -0,0 +1,25 @@
+package leetcode.jpmorgan;
+
+public class LowestCommonAncestorOfABinaryTree {
+
+    private static final class TreeNode {
+        int val;
+        TreeNode left;
+        TreeNode right;
+    }
+
+    public TreeNode lowestCommonAncestor(TreeNode root, TreeNode p, TreeNode q) {
+        if (root == null || root == p || root == q) {
+            return root;
+        }
+
+        TreeNode left = lowestCommonAncestor(root.left, p, q);
+        TreeNode right = lowestCommonAncestor(root.right, p, q);
+
+        if (left != null && right != null) {
+            return root;
+        }
+
+        return left == null ? right : left;
+    }
+}
