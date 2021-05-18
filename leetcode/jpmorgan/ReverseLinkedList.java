202318df1c6c15b085391f64ac4aa78a758efe39
Lakshmikant Deshpande <lakshmikantdeshpand@gmail.com>
2021-05-18 08:32:03 +0530

    https://leetcode.com/problems/reverse-linked-list/

---
+++ leetcode/jpmorgan/ReverseLinkedList.java
@@ -0,0 +1,31 @@
+package leetcode.jpmorgan;
+
+public class ReverseLinkedList {
+
+    private static final class ListNode {
+        int val;
+        ListNode next;
+
+        ListNode(int x) {
+            val = x;
+            next = null;
+        }
+    }
+
+    public ListNode reverseList(ListNode head) {
+        if (head == null) {
+            return null;
+        }
+
+        ListNode prev = null;
+        ListNode curr = head;
+
+        while (curr != null) {
+            ListNode next = curr.next;
+            curr.next = prev;
+            prev = curr;
+            curr = next;
+        }
+        return prev;
+    }
+}
