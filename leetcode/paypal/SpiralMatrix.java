0383166232023350206134efe02ef65fc1783f50
Lakshmikant Deshpande <lakshmikantdeshpand@gmail.com>
2021-06-08 23:35:56 +0530

    https://leetcode.com/problems/spiral-matrix/

---
+++ leetcode/paypal/SpiralMatrix.java
@@ -0,0 +1,42 @@
+package leetcode.paypal;
+
+import java.util.ArrayList;
+import java.util.List;
+
+public class SpiralMatrix {
+    public List<Integer> spiralOrder(int[][] matrix) {
+        List<Integer> result = new ArrayList<>();
+
+        int rowBegin = 0;
+        int rowEnd = matrix.length - 1;
+        int colBegin = 0;
+        int colEnd = matrix[0].length - 1;
+
+        while (rowBegin <= rowEnd && colBegin <= colEnd) {
+            for (int i = colBegin; i <= colEnd; i++) {
+                result.add(matrix[rowBegin][i]);
+            }
+            rowBegin++;
+
+            for (int i = rowBegin; i <= rowEnd; i++) {
+                result.add(matrix[i][colEnd]);
+            }
+            colEnd--;
+
+            if (rowBegin <= rowEnd) {
+                for (int i = colEnd; i >= colBegin; i--) {
+                    result.add(matrix[rowEnd][i]);
+                }
+            }
+            rowEnd--;
+
+            if (colBegin <= colEnd) {
+                for (int i = rowEnd; i >= rowBegin; i--) {
+                    result.add(matrix[i][colBegin]);
+                }
+            }
+            colBegin++;
+        }
+        return result;
+    }
+}
