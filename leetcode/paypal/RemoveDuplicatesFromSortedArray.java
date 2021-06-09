8f2d9f6e32486bb9ddbf45877be35417d02621c7
Lakshmikant Deshpande <lakshmikantdeshpand@gmail.com>
2021-06-09 08:35:14 +0530

    https://leetcode.com/problems/remove-duplicates-from-sorted-array/

---
+++ leetcode/paypal/RemoveDuplicatesFromSortedArray.java
@@ -0,0 +1,19 @@
+package leetcode.paypal;
+
+public class RemoveDuplicatesFromSortedArray {
+    public int removeDuplicates(int[] nums) {
+        if (nums.length <= 1) {
+            return nums.length;
+        }
+
+        int i = 0;
+
+        for (int j = 1; j < nums.length; j++) {
+            if (nums[j] != nums[i]) {
+                i++;
+                nums[i] = nums[j];
+            }
+        }
+        return i + 1;
+    }
+}
